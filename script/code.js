dc25bd0f699c337138349dbbbc0a4bec5eb3f7d0
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-27 10:16:00 +0200

    patch 4

--- script/code.js
+++ script/code.js
@@ -1,10 +1,14 @@
+
+//creating const variables for output and button
 const conversion = document.querySelector('[data-submit]')
 const results = document.querySelector('#output') 
 
+//function
 function value (){
     let minutes = document.getElementById('minutes').value
     let seconds = minutes * 60
+    //toFixed is so there is 2 decimal places if needed 
     results.textContent = seconds.toFixed(2)
 }
-
+//button to do conversion and apply function
 conversion.addEventListener('click',value)
